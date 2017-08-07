d9c82c9ded6f8dbe373744b318ff227683ef739e
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-08-08 00:43:55 +0900

    Refactor 2d.ts

--- src/2d.ts
+++ src/2d.ts
@@ -1,47 +1,47 @@
 // Reference : http://www.cam.hi-ho.ne.jp/strong_warriors/teacher/chapter0{4,5}.html
 
-class Point {
-	constructor(public x: number, public y: number) {
-	}
+/** Cross product of two vectors */
+function cross(x1: number, y1: number, x2: number, y2: number) {
+	return x1 * y2 - x2 * y1;
 }
 
-function getCrossPoint(
-	x11: number, y11: number, x12: number, y12: number,
-	x21: number, y21: number, x22: number, y22: number) {
-	const a1 = y12 - y11;
-	const b1 = x11 - x12;
-	const c1 = -1 * a1 * x11 - b1 * y11;
-	const a2 = y22 - y21;
-	const b2 = x21 - x22;
-	const c2 = -1 * a2 * x21 - b2 * y21;
+// class Point {
+// 	constructor(public x: number, public y: number) {
+// 	}
+// }
 
-	const temp = b1 * a2 - b2 * a1;
-	if (temp === 0) { // parallel
-		return null;
-	}
-	return new Point((c1 * b2 - c2 * b1) / temp, (a1 * c2 - a2 * c1) / temp);
-}
+// function getCrossPoint(
+// 	x11: number, y11: number, x12: number, y12: number,
+// 	x21: number, y21: number, x22: number, y22: number) {
+// 	const a1 = y12 - y11;
+// 	const b1 = x11 - x12;
+// 	const c1 = -1 * a1 * x11 - b1 * y11;
+// 	const a2 = y22 - y21;
+// 	const b2 = x21 - x22;
+// 	const c2 = -1 * a2 * x21 - b2 * y21;
+//
+// 	const temp = b1 * a2 - b2 * a1;
+// 	if (temp === 0) { // parallel
+// 		return null;
+// 	}
+// 	return new Point((c1 * b2 - c2 * b1) / temp, (a1 * c2 - a2 * c1) / temp);
+// }
 
 function isCross(
 	x11: number, y11: number, x12: number, y12: number,
 	x21: number, y21: number, x22: number, y22: number) {
-	const temp = getCrossPoint(x11, y11, x12, y12, x21, y21, x22, y22);
-	if (!temp) {
-		return false;
+	const cross_1112_2122 = cross(x12 - x11, y12 - y11, x22 - x21, y22 - y21);
+	if (cross_1112_2122 === 0) {
+		// parallel
+		return false; // XXX should check if segments overlap?
 	}
-	if (x11 < x12 && (temp.x < x11 || x12 < temp.x)
-		|| x11 > x12 && (temp.x < x12 || x11 < temp.x)
-		|| y11 < y12 && (temp.y < y11 || y12 < temp.y)
-		|| y11 > y12 && (temp.y < y12 || y11 < temp.y)) {
-		return false;
-	}
-	if (x21 < x22 && (temp.x < x21 || x22 < temp.x)
-		|| x21 > x22 && (temp.x < x22 || x21 < temp.x)
-		|| y21 < y22 && (temp.y < y21 || y22 < temp.y)
-		|| y21 > y22 && (temp.y < y22 || y21 < temp.y)) {
-		return false;
-	}
-	return true;
+
+	const cross_1112_1121 = cross(x12 - x11, y12 - y11, x21 - x11, y21 - y11);
+	const cross_1112_1122 = cross(x12 - x11, y12 - y11, x22 - x11, y22 - y11);
+	const cross_2122_2111 = cross(x22 - x21, y22 - y21, x11 - x21, y11 - y21);
+	const cross_2122_2112 = cross(x22 - x21, y22 - y21, x12 - x21, y12 - y21);
+
+	return cross_1112_1121 * cross_1112_1122 <= 0 && cross_2122_2111 * cross_2122_2112 <= 0;
 }
 
 function isCrossBox(
