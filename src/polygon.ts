53c34a77dca31a9ceb477713abe0549412ed8998
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-08-16 11:19:49 +0900

    Add geometric methods to Polygon

--- src/polygon.ts
+++ src/polygon.ts
@@ -1,13 +1,21 @@
 export class Polygon {
 	// resolution : 0.1
-	public array: Array<{x: number; y: number; off: boolean; }>;
-	constructor(number?: number) {
-	// property
+	public array: Array<{ x: number; y: number; off: boolean; }>;
+	constructor(number?: number);
+	constructor(array: Array<{ x: number; y: number; off?: boolean; }>);
+	constructor(param?: number | Array<{ x: number; y: number; off?: boolean; }>) {
+		// property
 		this.array = [];
 		// initialize
-		if (number) {
-			for (let i = 0; i < number; i++) {
-				this.push(0, 0, false);
+		if (param) {
+			if (typeof param === "number") {
+				for (let i = 0; i < param; i++) {
+					this.push(0, 0, false);
+				}
+			} else {
+				param.forEach(({ x, y, off }) => {
+					this.push(x, y, off);
+				});
 			}
 		}
 	}
@@ -45,6 +53,56 @@ export class Polygon {
 			y: Math.floor(y * 10) / 10,
 			off,
 		});
+	}
+
+	public clone() {
+		const newpolygon = new Polygon();
+		this.array.forEach(({ x, y, off }) => {
+			newpolygon.push(x, y, off);
+		});
+		return newpolygon;
+	}
+
+	public translate(dx: number, dy: number) {
+		this.array.forEach((point) => {
+			point.x += dx;
+			point.y += dy;
+		});
+		return this; // for chaining
+	}
+
+	public transformMatrix(a: number, b: number, c: number, d: number) {
+		this.array.forEach((point) => {
+			const { x, y } = point;
+			point.x = a * x + b * y;
+			point.y = c * x + d * y;
+		});
+		return this; // for chaining
+	}
+
+	/**
+	 * Scales by hypot(x, y) and rotates by atan2(y, x). Corresponds to multiplying x+yi on complex plane.
+	 */
+	public transformMatrix2(x: number, y: number) {
+		return this.transformMatrix(x, -y, y, x);
+	}
+
+	public reflectX() {
+		this.array.forEach((point) => {
+			point.x *= -1;
+		});
+		return this; // for chaining
+	}
+
+	public reflectY() {
+		this.array.forEach((point) => {
+			point.y *= -1;
+		});
+		return this; // for chaining
+	}
+
+	public rotate180() {
+		return this.reflectX().reflectY(); // for chaining
 	}
 
 }
