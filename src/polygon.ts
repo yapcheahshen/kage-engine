2c8ace1a464a8b355e1e129f9a54ad25984f604a
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-08-16 14:00:47 +0900

    Make Polygon#array getter and do floor in the getter

--- src/polygon.ts
+++ src/polygon.ts
@@ -1,11 +1,18 @@
 export class Polygon {
 	// resolution : 0.1
-	public array: Array<{ x: number; y: number; off: boolean; }>;
+	public get array(): ReadonlyArray<Readonly<{x: number, y: number, off: boolean}>> {
+		return this._array.map(({ x, y, off }) => ({
+			x: Math.floor(x * 10) / 10, // should be Math.round?
+			y: Math.floor(y * 10) / 10, // should be Math.round?
+			off,
+		}));
+	}
+	private _array: Array<{ x: number; y: number; off: boolean; }>;
 	constructor(number?: number);
 	constructor(array: Array<{ x: number; y: number; off?: boolean; }>);
 	constructor(param?: number | Array<{ x: number; y: number; off?: boolean; }>) {
 		// property
-		this.array = [];
+		this._array = [];
 		// initialize
 		if (param) {
 			if (typeof param === "number") {
@@ -22,49 +29,41 @@ export class Polygon {
 
 	// method
 	public push(x: number, y: number, off: boolean = false) {
-		this.array.push({
-			x: Math.floor(x * 10) / 10,
-			y: Math.floor(y * 10) / 10,
-			off,
-		});
+		this._array.push({ x, y, off });
 	}
 
 	public set(index: number, x: number, y: number, off: boolean = false) {
-		this.array[index].x = Math.floor(x * 10) / 10;
-		this.array[index].y = Math.floor(y * 10) / 10;
-		this.array[index].off = off;
+		this._array[index].x = x;
+		this._array[index].y = y;
+		this._array[index].off = off;
 	}
 
 	public reverse() {
-		this.array.reverse();
+		this._array.reverse();
 	}
 
 	public concat(poly: Polygon) {
-		this.array = this.array.concat(poly.array);
+		this._array = this._array.concat(poly._array);
 	}
 
 	public shift() {
-		this.array.shift();
+		this._array.shift();
 	}
 
 	public unshift(x: number, y: number, off: boolean = false) {
-		this.array.unshift({
-			x: Math.floor(x * 10) / 10,
-			y: Math.floor(y * 10) / 10,
-			off,
-		});
+		this._array.unshift({ x, y, off });
 	}
 
 	public clone() {
 		const newpolygon = new Polygon();
-		this.array.forEach(({ x, y, off }) => {
+		this._array.forEach(({ x, y, off }) => {
 			newpolygon.push(x, y, off);
 		});
 		return newpolygon;
 	}
 
 	public translate(dx: number, dy: number) {
-		this.array.forEach((point) => {
+		this._array.forEach((point) => {
 			point.x += dx;
 			point.y += dy;
 		});
@@ -72,7 +71,7 @@ export class Polygon {
 	}
 
 	public transformMatrix(a: number, b: number, c: number, d: number) {
-		this.array.forEach((point) => {
+		this._array.forEach((point) => {
 			const { x, y } = point;
 			point.x = a * x + b * y;
 			point.y = c * x + d * y;
@@ -88,14 +87,14 @@ export class Polygon {
 	}
 
 	public reflectX() {
-		this.array.forEach((point) => {
+		this._array.forEach((point) => {
 			point.x *= -1;
 		});
 		return this; // for chaining
 	}
 
 	public reflectY() {
-		this.array.forEach((point) => {
+		this._array.forEach((point) => {
 			point.y *= -1;
 		});
 		return this; // for chaining
