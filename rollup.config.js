de71dfe4a3f0f3141048f717c9d403b65e1bb655
kurgm <gogottt2009-hp@yahoo.co.jp>
2020-07-17 01:22:34 +0900

    Setup rollup build

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+
+import pkg from "./package.json";
+
+const banner = `/*! kage.js v${pkg.version}
+ *  Licensed under ${pkg.license}
+ *  ${pkg.homepage}
+ */`;
+
+export default {
+	input: "src/browser.ts",
+	output: [
+		{
+			file: "dist/kage.js",
+			format: "iife",
+			name: "Kage",
+			exports: "default",
+			banner,
+		},
+		{
+			file: "dist/kage.min.js",
+			format: "iife",
+			name: "Kage",
+			exports: "default",
+			banner,
+			plugins: [terser()],
+		},
+	],
+	plugins: [typescript({ tsconfig: false, target: "es5" })],
+};
--- src/browser.ts
+++ src/browser.ts
@@ -1,12 +1,1 @@
-import { Kage as _Kage, Polygons as _Polygons, Buhin as _Buhin } from "./";
-
-declare interface Window {
-	Kage: typeof _Kage;
-	Polygons: typeof _Polygons;
-	Buhin: typeof _Buhin;
-}
-// eslint-disable-next-line no-var
-declare var window: Window;
-window.Kage = _Kage;
-window.Polygons = _Polygons;
-window.Buhin = _Buhin;
+export { default } from "./";
